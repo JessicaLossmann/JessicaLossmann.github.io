498093d2f0ffe84e4d473d17f1ac6c1853629635
JessicaLossmann <jwlossmann@my.waketech.edu>
2025-04-25 17:32:56 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -5,18 +5,18 @@ function fetchGradeData() {
   let xhr = new XMLHttpRequest();
   let apiRoute = "/api/grades";
 
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = function (){
       let results;
-    if (xhr.readyState === xhr.DONE) {
-      if (xhr.status !== 200) {
-        console.error(`Could not get grades. 
+    if (xhr.readyState === xhr.DONE){
+        if (xhr.status !== 200){
+             console.error(`Could not get grades. 
                 Status: ${xhr.status}`);
-       }
-      populateGradebook(JSON.parse(xhr.responseText));
+        }
+        populateGradebook(JSON.parse(xhr.responseText));
       }
-  }.bind(this);
-  xhr.open("getr", apiRoute, true);
-  xhr.send();
+   }.bind(this);
+   xhr.open("get", apiRoute, true);
+   xhr.send();
 }
 
 // Populate the table with grade data
